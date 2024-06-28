9f2994a79bc985547297f36e1ea8fa1a93b6943a
Web-Wss <1306822660@qq.com>
2024-06-28 10:53:05 +0800

    使用RTK管理账单列表

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+
 import router from "./router";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // 导入定制主题文件
 import "./theme.css";
@@ -13,4 +16,8 @@ const total = sum(1, 2);
 console.log(total);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
