2e50c007dcf43d78be551405b42c1b574f2d69dc
Web-Wss <1306822660@qq.com>
2024-06-28 15:04:46 +0800

    点击切换展示和收起状态-日账单

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,6 @@
 import classNames from "classnames";
 import "./index.scss";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 import { billTypeToName } from "@/contants";
 
@@ -19,12 +19,18 @@ const DailyBill = ({ date, billList }) => {
       total: pay + income,
     };
   }, [billList]);
+
+  // 控制展开收起
+  const [visible, setvisible] = useState(false);
   return (
     <div className={classNames("dailyBill")}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames("arrow")}></span>
+          <span
+            className={classNames("arrow", visible && "expand")}
+            onClick={() => setvisible(!visible)}
+          ></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -42,7 +48,7 @@ const DailyBill = ({ date, billList }) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
+      <div className="billList" style={{ display: visible ? "block" : "none" }}>
         {billList.map((item) => {
           return (
             <div className="bill" key={item.id}>
