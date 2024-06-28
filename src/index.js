0c7ed0ad0f753f54eb65a61aeba28378d84d1a90
Web-Wss <1306822660@qq.com>
2024-06-28 10:32:48 +0800

    完成主题色定制

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
+// 导入定制主题文件
+import "./theme.css";
+
 import sum from "@/test";
 
 const total = sum(1, 2);
