6b8b4f3089812b4afdd0d5e4b56bfcceaa1a7c06
Web-Wss <1306822660@qq.com>
2024-06-28 15:23:04 +0800

    记账模块静态页面完成

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -3,6 +3,7 @@ import "./index.scss";
 import { useMemo, useState } from "react";
 
 import { billTypeToName } from "@/contants";
+import Icon from "@/components/Icon";
 
 const DailyBill = ({ date, billList }) => {
   const dayResult = useMemo(() => {
@@ -52,6 +53,8 @@ const DailyBill = ({ date, billList }) => {
         {billList.map((item) => {
           return (
             <div className="bill" key={item.id}>
+              {/* 图标 */}
+              <Icon type={item.useFor} />
               <div className="detail">
                 <div className="billType">{billTypeToName[item.useFor]}</div>
               </div>
